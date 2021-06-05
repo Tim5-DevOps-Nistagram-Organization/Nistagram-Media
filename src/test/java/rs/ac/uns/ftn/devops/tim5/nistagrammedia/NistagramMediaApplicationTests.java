53164a1f06df3aee78d2cdaa8e92375353b880f2
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-05 16:50:56 +0200

    init project #6

---
+++ src/test/java/rs/ac/uns/ftn/devops/tim5/nistagrammedia/NistagramMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.devops.tim5.nistagrammedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NistagramMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
