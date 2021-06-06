d0917fef41aabf680b05d3a22aa6797924973bd6
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-06 18:15:00 +0200

    init project #6 (#8)

---
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagrammedia/NistagramMediaApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.devops.tim5.nistagrammedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NistagramMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NistagramMediaApplication.class, args);
+	}
+
+}
